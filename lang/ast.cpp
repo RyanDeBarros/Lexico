393b06204e6d2f41cdc5f15ebda1e5a6258187f6
RyanDeBarros <ryan2004db@gmail.com>
2026-04-22 23:32:07 -0400

    Restructured AST node design

--- lang/ast.cpp
+++ lang/ast.cpp
@@ -5,131 +5,6 @@
 
 namespace lx
 {
-	void ASTNode::append(ASTNode& child)
-	{
-		if (child._parent)
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": child already has a parent";
-			throw std::runtime_error(ss.str());
-		}
-
-		child._parent = this;
-		_children.push_back(&child);
-	}
-
-	size_t ASTNode::children_count() const
-	{
-		return _children.size();
-	}
-	
-	const ASTNode& ASTNode::child(size_t i) const
-	{
-		return *_children[i];
-	}
-
-	ASTNode& ASTNode::child(size_t i)
-	{
-		return *_children[i];
-	}
-
-	bool ASTNode::has_parent() const
-	{
-		return _parent != nullptr;
-	}
-
-	const ASTNode& ASTNode::parent() const
-	{
-		if (!has_parent())
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": no parent";
-			throw std::runtime_error(ss.str());
-		}
-
-		return *_parent;
-	}
-
-	ASTNode& ASTNode::parent()
-	{
-		if (!has_parent())
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": no parent";
-			throw std::runtime_error(ss.str());
-		}
-
-		return *_parent;
-	}
-
-	AbstractSyntaxTree::ConstIterator::ConstIterator(const AbstractSyntaxTree& tree)
-		: _tree(tree), _current_node(!tree._nodes.empty() ? _current_node = tree._nodes[0].get() : nullptr)
-	{
-	}
-
-	bool AbstractSyntaxTree::ConstIterator::eof() const
-	{
-		return _current_node != nullptr;
-	}
-	
-	void AbstractSyntaxTree::ConstIterator::advance()
-	{
-		if (eof())
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": at eof()";
-			throw std::runtime_error(ss.str());
-		}
-
-		if (_current_node->children_count() > 0)
-		{
-			_index_in_parent.push(0);
-			_current_node = &_current_node->child(0);
-		}
-		else
-		{
-			while (_current_node->has_parent())
-			{
-				_current_node = &_current_node->parent();
-				size_t sibling_index = _index_in_parent.top() + 1;
-				_index_in_parent.pop();
-				if (sibling_index < _current_node->children_count())
-				{
-					_index_in_parent.push(sibling_index);
-					_current_node = &_current_node->child(sibling_index);
-					break;
-				}
-			}
-
-			if (!_current_node->has_parent())
-				_current_node = nullptr;
-		}
-	}
-	
-	const ASTNode& AbstractSyntaxTree::ConstIterator::operator*() const
-	{
-		if (eof())
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": at eof()";
-			throw std::runtime_error(ss.str());
-		}
-
-		return *_current_node;
-	}
-	
-	const ASTNode* AbstractSyntaxTree::ConstIterator::operator->() const
-	{
-		if (eof())
-		{
-			std::stringstream ss;
-			ss << __FUNCTION__ << ": at eof()";
-			throw std::runtime_error(ss.str());
-		}
-
-		return _current_node;
-	}
-
 	ASTNode& AbstractSyntaxTree::add(std::unique_ptr<ASTNode>&& node)
 	{
 		ASTNode* ref = node.get();
@@ -137,8 +12,53 @@ namespace lx
 		return *ref;
 	}
 
-	AbstractSyntaxTree::ConstIterator AbstractSyntaxTree::iterator() const
+	const ASTNode& AbstractSyntaxTree::root() const
 	{
-		return ConstIterator(*this);
+		return *_nodes[0];
+	}
+
+	ASTNode& AbstractSyntaxTree::root()
+	{
+		return *_nodes[0];
+	}
+
+	void Block::append(ASTNode& child)
+	{
+		_children.push_back(&child);
+	}
+
+	VariableDeclaration::VariableDeclaration(Token&& declarer, Token&& identifier, Expression& expression)
+		: _declarer(std::move(declarer)), _identifier(std::move(identifier)), _expression(&expression)
+	{
+	}
+
+	bool VariableDeclaration::is_global() const
+	{
+		return _declarer.type == TokenType::Var;
+	}
+
+	const std::string& VariableDeclaration::variable_name() const
+	{
+		return _identifier.lexeme;
+	}
+
+	LiteralExpression::LiteralExpression(Token&& literal)
+		: _literal(std::move(literal))
+	{
+	}
+
+	BinaryExpression::BinaryExpression(Token&& op, Expression& left, Expression& right)
+		: _op(std::move(op)), _left(&left), _right(&right)
+	{
+	}
+
+	VariableExpression::VariableExpression(Token&& identifier)
+		: _identifier(std::move(identifier))
+	{
+	}
+
+	FunctionExpression::FunctionExpression(Token&& identifier, std::vector<Expression*>&& args)
+		: _identifier(std::move(identifier)), _args(std::move(args))
+	{
 	}
 }
--- lang/ast.h
+++ lang/ast.h
@@ -1,49 +1,84 @@
 #pragma once
 
 #include <memory>
-#include <vector>
-#include <stack>
+
+#include "token.h"
 
 namespace lx
 {
-	class ASTNode
+	struct ASTNode
 	{
-		ASTNode* _parent = nullptr;
-		std::vector<ASTNode*> _children;
-
-	public:
 		virtual ~ASTNode() = default;
-
-		void append(ASTNode& child);
-		size_t children_count() const;
-		const ASTNode& child(size_t i) const;
-		ASTNode& child(size_t i);
-		bool has_parent() const;
-		const ASTNode& parent() const;
-		ASTNode& parent();
 	};
 
 	class AbstractSyntaxTree
 	{
 		std::vector<std::unique_ptr<ASTNode>> _nodes;
 
-		class ConstIterator
-		{
-			const AbstractSyntaxTree& _tree;
-			std::stack<size_t> _index_in_parent;
-			ASTNode* _current_node;
-
-		public:
-			ConstIterator(const AbstractSyntaxTree& tree);
-			
-			bool eof() const;
-			void advance();
-			const ASTNode& operator*() const;
-			const ASTNode* operator->() const;
-		};
-
 	public:
 		ASTNode& add(std::unique_ptr<ASTNode>&& node);
-		ConstIterator iterator() const;
+		const ASTNode& root() const;
+		ASTNode& root();
+	};
+
+	class Block : public ASTNode
+	{
+		std::vector<ASTNode*> _children;
+
+	public:
+		void append(ASTNode& child);
+	};
+
+	struct Expression : public ASTNode
+	{
+		// TODO virtual value variant
+	};
+
+	class VariableDeclaration : public ASTNode
+	{
+		Token _declarer;
+		Token _identifier;
+		Expression* _expression;
+
+	public:
+		VariableDeclaration(Token&& declarer, Token&& identifier, Expression& expression);
+		
+		bool is_global() const;
+		const std::string& variable_name() const;
+	};
+
+	class LiteralExpression : public Expression
+	{
+		Token _literal;
+
+	public:
+		LiteralExpression(Token&& literal);
+	};
+
+	class BinaryExpression : public Expression
+	{
+		Token _op;
+		Expression* _left;
+		Expression* _right;
+
+	public:
+		BinaryExpression(Token&& op, Expression& left, Expression& right);
+	};
+
+	class VariableExpression : public Expression
+	{
+		Token _identifier;
+
+	public:
+		VariableExpression(Token&& identifier);
+	};
+
+	class FunctionExpression : public Expression
+	{
+		Token _identifier;
+		std::vector<Expression*> _args;
+
+	public:
+		FunctionExpression(Token&& identifier, std::vector<Expression*>&& args);
 	};
 }
